d82857ab34b13e8d42fbe62f9be2753128c46bb3
laraphasha <rapelanglara23@gmail.com>
2025-07-13 15:16:04 +0200

    square

---
+++ 8-square.js
@@ -0,0 +1,16 @@
+const { argv } = require('node:process');
+
+let n_times = parseInt(argv[2])
+
+if (n_times){
+  for (let i = n_times; i > 0; i--) {
+    let x = ""
+    for (let j = n_times; j > 0; j--) {
+      x = x + "X"
+    }
+    console.log(x)
+  }
+}
+else {
+  console.log(argv[2] + "\nMissing size")
+}
