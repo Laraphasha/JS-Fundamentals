ec0fdff0533578530cb0a289e2ff533c0e4dc7f4
laraphasha <rapelanglara23@gmail.com>
2025-07-13 17:36:24 +0200

    add

---
+++ 9-add.js
@@ -0,0 +1,10 @@
+const { argv } = require('node:process');
+
+function add(a, b) {
+  console.log(a + b)
+}
+
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+
+add(first, second);
